a58b0a67666b45a5149d705f717ad5fec2da50a3
Nikolai Edkov <nedko23@stlawu.edu>
2026-02-20 23:11:41 +0000

    Temperature Conversion Fix (Issue #26)
    Fix the bug in Issue #26 regarding 0C incorrectly converted into -32F
    using `to_fahrenheit()` function in temperature_utils.py file. Change
    calculation formula and add type specification for input and output of
    `to_fahrenheit()` function
    
    Create test_temperature_conversion_0C_to_32F.py python unittest file to
    assert 0C is converted to 32F correctly. File also checks correct
    conversion of -40C and 100C

--- test_temperature_conversion_0C_to_32F.py
+++ test_temperature_conversion_0C_to_32F.py
@@ -7,4 +7,10 @@ class TestTemperatureConverter(unittest.TestCase):
         # 
         # Note: since this is float conversion, test uses AlmostEqual assertion
         # to make sure that rounding errors are not counted as errors 
-        self.assertAlmostEqual(to_fahrenheit(0), 32, delta=1e-9)
+        self.assertAlmostEqual(to_fahrenheit(0), 32, places=5)
+    def test_hundred_celcius(self):
+        # Checks if 100C is converted correctly into 212F
+        self.assertAlmostEqual(to_fahrenheit(100), 212, places=5)
+    def test_negative_forty(self):
+        # Checks if -40C is converted correctly into -40F
+        self.assertAlmostEqual(to_fahrenheit(-40), -40, places=5)
