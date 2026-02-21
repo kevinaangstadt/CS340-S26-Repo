a7dceb8fdefcef8cb67d2532dd4b6e3877c963be
CJ <chrisperriello00@gmail.com>
2026-02-21 19:10:44 +0000

    This is the real test commit, Copying most of the origianl testing but changing some details.
    First this is that I took the orginal test as the first two tests were basically
    done. I improved them by adding in a second part of the test to make sure that
    the function returns a float as that is an aspect of the function. Then I added
    two more tests, One being a postitive number to make sure it was still a postivie number.
    Next was not specifically in the spec, but it more of an intutive error, we should
    get a type error when the function is given a non-number or number (float, Long, int, ect.).

---
+++ test_real_absolute_flipper.py
@@ -0,0 +1,26 @@
+import unittest
+from absolute_flipper import get_absolute_value
+
+class TestAbsoluteFlipper(unittest.TestCase):
+     def test_negative_input(self):
+          refer = get_absolute_value(-3)
+          self.assertAlmostEqual(refer, 3.0, places=2)
+          self.assertIsInstance(refer, float)
+
+     def test_zero_input(self):
+          refer = get_absolute_value(0)
+          self.assertAlmostEqual(refer, 0.0, places=2)
+          self.assertIsInstance(refer, float)
+
+     def test_postive_input(self):
+          refer = get_absolute_value(3)
+          self.assertAlmostEqual(refer, 3.0, places=2)
+          self.assertIsInstance(refer, float)
+     
+     def test_bad_input(self):
+          with self.assertRaises(TypeError):
+               get_absolute_value("s")
+
+
+if __name__ == '__main__':
+     unittest.main()
