4a3e7a463fa1f944c03bbf006a104c3f314d065c
George Chang <gchan22@stlawu.edu>
2026-02-23 13:36:53 +0000

    Bug fix where radius is not 2
    As there is a bug when the radius is not 2 I used made the radius
    3 for my new test. For the new test I ran it succeded in the test.
    The big was that the formula was incorrect for the program so
    correcting the program allowed radius other than 2 to work.

--- test_circle_area2.py
+++ test_circle_area2.py
@@ -3,7 +3,7 @@ from circle_area import calculate_area
 
 class TestCircleArea(unittest.TestCase):
     def test_radius_two(self):
-        self.assertAlmostEqual(calculate_area(2), 12.56)
+        self.assertAlmostEqual(calculate_area(3), 28.26)
 
 if __name__ == '__main__':
     unittest.main()
