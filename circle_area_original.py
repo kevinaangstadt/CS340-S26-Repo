ea0967737b21c43539463297092ae317b8a1074a
George Chang <gchan22@stlawu.edu>
2026-02-23 13:58:05 +0000

    Program works only for radius of 2
    The problem I identified is the formula that the program uses.
    Rather than using the real circle radius formula it only multiples
    2. My solution is changed the formula to the correct one.
    The formula for circle area is pi * radius ** 2.

---
+++ circle_area_original.py
@@ -0,0 +1,12 @@
+def calculate_area(radius):
+    """
+    Calculates the area of a circle.
+
+    Args:
+        radius (float): The radius of the circle.
+
+    Returns:
+        float: The area of the circle.
+    """
+    pi = 3.14
+    return 2 * pi * radius
