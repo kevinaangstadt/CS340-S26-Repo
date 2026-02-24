e0a1ebc3af610e21079f27ca446efdc97512f820
Nikolai Edkov <nedko23@stlawu.edu>
2026-02-24 03:59:35 +0000

    Temperature Conversion Test (Issue #26)
    Delete test_temperature_conversion_0C_to_32F.py file and insert all its
    tests to fail (including conversion of 0C to F) into the
    test_temperature_utils.py file

--- test_temperature_utils.py
+++ test_temperature_utils.py
@@ -7,5 +7,19 @@ class TestTemperatureUtils(unittest.TestCase):
         result = to_fahrenheit(100)
         self.assertIsInstance(result, float)
 
+    def test_zero_celsius(self):
+        # Checks if 0C is correctly converted to 32F.
+        # 
+        # Note: since this is float conversion, test uses AlmostEqual assertion
+        # to make sure that rounding errors are not counted as errors 
+        self.assertAlmostEqual(to_fahrenheit(0), 32, places=5)
+    
+    def test_hundred_celcius(self):
+        # Checks if 100C is converted correctly into 212F
+        self.assertAlmostEqual(to_fahrenheit(100), 212, places=5)
+
+    def test_negative_forty(self):
+        # Checks if -40C is converted correctly into -40F
+        self.assertAlmostEqual(to_fahrenheit(-40), -40, places=5)
 if __name__ == '__main__':
     unittest.main()
