188bf89f7dc422e585f8b0bf497eaf59b43aa9ac
Siadhal Sweeney <sswee23@stlawu.edu>
2026-02-28 01:10:34 +0000

    Add a test for a list with all negatives
    Made a short test with for negative numbers the max being -2
    so expected was -2. When ran it Failed.
    
    Fix #36

--- test_list_max_finder.py
+++ test_list_max_finder.py
@@ -8,5 +8,8 @@ class TestListMaxFinder(unittest.TestCase):
     def test_mixed_numbers(self):
         self.assertEqual(find_max([-10, 5, 2]), 5)
 
+    def test_all_negative_numbers(self):
+        self.assertEqual(find_max([-5,-2,-9,-11]),-2)
+
 if __name__ == '__main__':
     unittest.main()
