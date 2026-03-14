25a42f21b30466efb0366e5713d9affc1cdd16d3
Oakley Lindsay <123098548+oelind@users.noreply.github.com>
2026-03-14 19:39:17 -0400

    Update color_converter.py
    Added if statements to add leading zeros for cases that any of the values may be less than 16 (ie when a leading zero would be included since two numbers are not absolutely necessary to represent the number). I used only if statements since I do not know of how to properly order all the potential combinations in a way so numerically there would be instances where one of the received parameter's values would be above a value besides when r, g, and b are all above 15. 
    
    To get around the possibility of more than one if statement being applicable per case I used and statements to specify the ranges each case could be.
    
    Hopefully the syntax of where the leading zeros are added works correctly based on the sources mentioned in the comments near the beginning of the function (specifically the second where the dollar sign is added) for how the function worked (I did somehow miss the implied bit where a string is formatted by being oblivious to the quotation marks around the syntax in front of the .format until after checking the first resource).

--- color_converter.py
+++ color_converter.py
@@ -10,4 +10,51 @@ def rgb_to_hex(r, g, b):
     Returns:
         str: The hexadecimal color string (e.g., "#ffffff").
     """
-    return "#{:x}{:x}{:x}".format(r, g, b)
+# https://python-academy.org/en/handbook/format
+# according to the souce above the format function formats strings
+# so to catch the corner case of any rgb value being 15 or less a 0 can be 
+# added to the output
+
+# https://realpython.com/ref/builtin-functions/format/
+# based on the source above the added zeros could be put outside the curly braces
+# at least that is my current theory at the moment
+    
+    #adding if statments to include leading zeros if there can be any
+    #if any of the rgb values are 15 or less then a leading zero will be added
+    #since the format function seems to not add them- thus they will be a hardcoded addition\
+
+    #chceking if all values are less than 16
+    if (r <= 15 and g <= 15 and b <=15):
+        return "#0{:x}0{:x}0{:x}".format(r, g, b)
+        
+    #checking if only r and g values are less than 16 
+    #(including  requirement of b being greater than 15 to prevent 
+    #this statment also being used after the statment above)
+    if(r <= 15 and g <= 15 and b > 15):
+        return "#0{:x}0{:x}{:x}".format(r, g, b)
+
+    #checking if only g and b are less than 16
+    if(r > 15 and g <= 15 and b <= 15):
+        return "#{:x}0{:x}0{:x}".format(r, g, b)
+        
+    #checking if only r and b are less than 16
+    if(r <= 15 and g > 15 and b <= 15):
+        return "#0{:x}{:x}0{:x}".format(r, g, b)
+
+    #checking if only r is less than 16
+    if(r <= 15 and g > 15 and b > 15):
+            return "#0{:x}{:x}{:x}".format(r, g, b)
+
+    #checking if only g is less than 16
+    if(r > 15 and g <= 15 and b > 15):
+        return "#{:x}0{:x}{:x}".format(r, g, b)
+
+    #checking if only b is less than 16
+    if(r > 15 and g > 15 and b <= 15):
+        return "#{:x}{:x}0{:x}".format(r, g, b)
+
+    #it is assumed if we get this far that all values are above 15
+    else:
+    #if any rgb value is greater than 15 then the code here works (all other code
+    #has been added)
+        return "#{:x}{:x}{:x}".format(r, g, b)
